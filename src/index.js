2229ab62db060288e3f7da570bfb15526866e7d3
rudotino <rtinofireyi@gmail.com>
2024-04-09 15:52:40 +0200

    Injected the forecastHtml from JavaScript to show the days

--- src/index.js
+++ src/index.js
@@ -52,8 +52,34 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+//
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-temperatures">
+      <span class="weather-forecast-temperature-max">18°</span>
+
+      <span class="weather-focus-temperature-min"> 12°</span>
+    </div>
+  </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+//
+displayForecast();
